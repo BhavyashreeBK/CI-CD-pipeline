4c074bf47723fd320567fe6a9c7a73b7e9b1b933
BhavyashreeBK <bkbhavyashree2@gmail.com>
2026-03-12 15:32:36 +0530

    Change header from h1 to h2 for Vite + React

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>welcome</h1>
-      <h1>Vite + React</h1>
+      <h2>Vite + React</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
